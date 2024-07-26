e00514262f85145742d1adbac000239079310f05
kj16609 <bigskyman02@gmail.com>
2024-07-26 16:05:55 -0400

    Actually do release builds

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ clear
 #time ./build/Main
 
 echo 3 > /proc/sys/vm/drop_caches
-cmake -DCMAKE_BUILD_TYPE=Debug -B build && \
+cmake -DCMAKE_BUILD_TYPE=Release -B build && \
 cmake --build build -j32 && \
 time ./build/Main > /dev/null
 
